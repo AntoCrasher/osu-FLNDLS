48e3f34636baf169733a2714c6ee80fd4779e43e
AntoCrasher <48983909+AntoCrasher@users.noreply.github.com>
2024-09-25 10:07:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def color_text(text, color=''):
     if color == 'ERROR':
         return f'\033[38;2;255;50;50m{text}\033[0m'
     else:
-        return f'\033[38;2;255;255;255mz{text}\033[0m'
+        return f'\033[38;2;255;255;255m{text}\033[0m'
 
 def FLNDLS(songs_path, beatmapset_path, beatmap_name, p_index, p_max, harder=False):
     buff = '-'
@@ -59,17 +59,21 @@ def FLNDLS(songs_path, beatmapset_path, beatmap_name, p_index, p_max, harder=Fal
         return
 
     if beatmap_name.count('[') > 0:
-        new_beatmap_name = f'{beatmap_name.split('[')[0]}[FLNDLS{buff} - {beatmap_name.split('[')[1]}'
+        new_beatmap_name = f'{beatmap_name.split("[")[0]}[FLNDLS{buff}_{beatmap_name.split("[")[1]}'
     elif beatmap_name.count('(') > 0:
-        new_beatmap_name = f'{beatmap_name.split('(')[-2]}[FLNDLS]{buff} ({beatmap_name.split('(')[-1]}'
+        new_beatmap_name = f'{beatmap_name.split("(")[-2]}[FLNDLS{buff}] ({beatmap_name.split("(")[-1]}'
     else:
-        new_beatmap_name = f'{beatmap_name.split('.osu')[0]}_FLNDLS{buff}.osu'
+        new_beatmap_name = f'{beatmap_name.split(".osu")[0]}_FLNDLS{buff}.osu'
 
     new_beatmap_path = f'{beatmapset_path}/{new_beatmap_name}'
     new_data = beatmap_data
 
     version = get(beatmap_data, 'Version')
-    if f'FLNDLS{buff} - ' in version:
+    if f'FLNDLS+ | ' in version:
+        print(progress + color_text(f'Map already FLNDLS+: {beatmap_name}', 'WARNING'))
+        return
+    if f'FLNDLS- | ' in version:
+        print(progress + color_text(f'Map already FLNDLS-: {beatmap_name}', 'WARNING'))
         return
 
     new_version = f'FLNDLS{buff} | ' + version
@@ -135,45 +139,59 @@ def FLNDLS(songs_path, beatmapset_path, beatmap_name, p_index, p_max, harder=Fal
             new_data += c_hit_object + '\n'
 
     if changes == 0:
-        print(progress + color_text(f'Already FLNDLS{buff}: {beatmap_name}', 'WARNING'))
+        print(progress + color_text(f'Map already Inverse{buff}: {beatmap_name}', 'WARNING'))
         return
 
     with open(new_beatmap_path, 'w', encoding='utf-8') as file:
         file.write(new_data)
-    print(progress + color_text(f'Done: {beatmap_name}', 'ACCEPT'))
+    print(progress + color_text(f'Done (FLNDLS{buff}): {beatmap_name}', 'ACCEPT'))
 
 def main():
     songs_path = 'C:/Users/anton/AppData/Local/osu!/Songs'
     beatmapsets = os.listdir(songs_path)
     index = 0
-
+    beatmap_count = 0
     for beatmapset_name in beatmapsets:
         beatmapset_path = f'{songs_path}/{beatmapset_name}'
         if not os.path.exists(beatmapset_path):
             continue
-        index += 1
-        # if index < 6600:
-        #     continue
         beatmaps = os.listdir(beatmapset_path)
         for beatmap_name in beatmaps:
             if not beatmap_name.endswith('.osu'):
                 continue
-
-            FLNDLS(songs_path, beatmapset_path, beatmap_name, index, len(beatmapsets), False)
+            beatmap_count += 1
+    for beatmapset_name in beatmapsets:
+        beatmapset_path = f'{songs_path}/{beatmapset_name}'
+        if not os.path.exists(beatmapset_path):
+            continue
+        beatmaps = os.listdir(beatmapset_path)
+        for beatmap_name in beatmaps:
+            if not beatmap_name.endswith('.osu'):
+                continue
+            index += 1
+            FLNDLS(songs_path, beatmapset_path, beatmap_name, index, beatmap_count * 2, False)
+            index += 1
+            FLNDLS(songs_path, beatmapset_path, beatmap_name, index, beatmap_count * 2, True)
 
 def single():
     songs_path = 'C:/Users/anton/AppData/Local/osu!/Songs'
     beatmapset_name = 'beatmap-638592658476932137-audio'
     beatmapset_path = f'{songs_path}/{beatmapset_name}'
     beatmaps = os.listdir(beatmapset_path)
+    index = 0
+    beatmap_count = 0
+    for beatmap_name in beatmaps:
+        if not beatmap_name.endswith('.osu'):
+            continue
+        beatmap_count += 1
     for beatmap_name in beatmaps:
         if not beatmap_name.endswith('.osu'):
             continue
 
-        FLNDLS(songs_path, beatmapset_path, beatmap_name, 1, 1, False)
-        FLNDLS(songs_path, beatmapset_path, beatmap_name, 1, 1, True)
+        index += 1
+        FLNDLS(songs_path, beatmapset_path, beatmap_name, index, beatmap_count * 2, False)
+        index += 1
+        FLNDLS(songs_path, beatmapset_path, beatmap_name, index, beatmap_count * 2, True)
 
-
-# main()
-single()
-
+main()
+# single()
